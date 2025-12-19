d9acae9f137df7fec20a7a910223f18d2a267059
aksaaa <akbarrajasa2@gmail.com>
2025-12-19 10:56:14 +0700

    praktikum 1

---
+++ PRAKTIKUM1PP.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "M. Akbar Rajasa";
+    string NPM = "2517051051";
+
+    cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+    cout << "Nama saya adalah " << nama << endl;
+    cout << "NPM saya adalah " << NPM << endl << endl;
+
+    cout << "Unila Indah" << endl;
+    cout << "Be Strong" << endl;
+}
